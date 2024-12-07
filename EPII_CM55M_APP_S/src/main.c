14afd68c8fbc7b95707a946dd14078626bef5ee9
Josuah Demangeon <me@josuah.net>
2024-12-07 10:36:36 +0000

    small cleanups

--- EPII_CM55M_APP_S/src/main.c
+++ EPII_CM55M_APP_S/src/main.c
@@ -105,11 +105,10 @@ __NO_RETURN void Reset_Handler(void)
 	SCB_EnableICache();
 	SCB_EnableDCache();
 
-
 	__PROGRAM_START();
 }
 
-__NO_RETURN void Default_Handler(void)
+void Default_Handler(void)
 {
 }
 
@@ -135,35 +134,35 @@ void Interrupt8_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
 void Interrupt9_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
 
 const VECTOR_TABLE_Type __VECTOR_TABLE[496] __VECTOR_TABLE_ATTRIBUTE = {
-	   (VECTOR_TABLE_Type)(&__INITIAL_SP), /*	 Initial Stack Pointer */
-	   Reset_Handler, /*	 Reset Handler */
-	   NMI_Handler, /* -14 NMI Handler */
-	   HardFault_Handler, /* -13 Hard Fault Handler */
-	   MemManage_Handler, /* -12 MPU Fault Handler */
-	   BusFault_Handler, /* -11 Bus Fault Handler */
-	   UsageFault_Handler, /* -10 Usage Fault Handler */
-	   SecureFault_Handler, /*  -9 Secure Fault Handler */
-	   0, /*	 Reserved */
-	   0, /*	 Reserved */
-	   0, /*	 Reserved */
-	   SVC_Handler, /*  -5 SVCall Handler */
-	   DebugMon_Handler, /*  -4 Debug Monitor Handler */
-	   0, /*	 Reserved */
-	   PendSV_Handler, /*  -2 PendSV Handler */
-	   SysTick_Handler, /*  -1 SysTick Handler */
+	(VECTOR_TABLE_Type)(&__INITIAL_SP), /* Initial Stack Pointer */
+	Reset_Handler,		/*  Reset Handler */
+	NMI_Handler,		/* -14 NMI Handler */
+	HardFault_Handler,	/* -13 Hard Fault Handler */
+	MemManage_Handler,	/* -12 MPU Fault Handler */
+	BusFault_Handler,	/* -11 Bus Fault Handler */
+	UsageFault_Handler,	/* -10 Usage Fault Handler */
+	SecureFault_Handler,	/* -9 Secure Fault Handler */
+	0,			/* Reserved */
+	0,			/* Reserved */
+	0,			/* Reservedn */
+	SVC_Handler,		/* -5 SVCall Handler */
+	DebugMon_Handler,	/* -4 Debug Monitor Handler */
+	0,			/* Reserved */
+	PendSV_Handler,		/* -2 PendSV Handler */
+	SysTick_Handler,	/* -1 SysTick Handler */
 
-	   /* Interrupts */
-	   Interrupt0_Handler, /*   0 Interrupt 0 */
-	   Interrupt1_Handler, /*   1 Interrupt 1 */
-	   Interrupt2_Handler, /*   2 Interrupt 2 */
-	   Interrupt3_Handler, /*   3 Interrupt 3 */
-	   Interrupt4_Handler, /*   4 Interrupt 4 */
-	   Interrupt5_Handler, /*   5 Interrupt 5 */
-	   Interrupt6_Handler, /*   6 Interrupt 6 */
-	   Interrupt7_Handler, /*   7 Interrupt 7 */
-	   Interrupt8_Handler, /*   8 Interrupt 8 */
-	   Interrupt9_Handler /*   9 Interrupt 9 */
-	   /* Interrupts 10 .. 480 are left out */
+	/* Interrupts */
+	Interrupt0_Handler,	/* 0 Interrupt 0 */
+	Interrupt1_Handler,	/* 1 Interrupt 1 */
+	Interrupt2_Handler,	/* 2 Interrupt 2 */
+	Interrupt3_Handler,	/* 3 Interrupt 3 */
+	Interrupt4_Handler,	/* 4 Interrupt 4 */
+	Interrupt5_Handler,	/* 5 Interrupt 5 */
+	Interrupt6_Handler,	/* 6 Interrupt 6 */
+	Interrupt7_Handler,	/* 7 Interrupt 7 */
+	Interrupt8_Handler,	/* 8 Interrupt 8 */
+	Interrupt9_Handler,	/*  9 Interrupt 9 */
+	/* Interrupts 10 .. 480 are left out */
 };
 
 uint32_t SystemCoreClock = SYSTEM_CLOCK;
@@ -175,85 +174,48 @@ void SystemCoreClockInit(void)
 	uint32_t val = SysTick_LOAD_RELOAD_Msk+1;
 	SystemCoreClock = SYSTEM_CLOCK;
 	g_time_loop = 0;
-#if !defined(ENABLE_OS) && !defined(RTE_CMSIS_RTOS2) && !defined(RTE_RTOS_FreeRTOS_CORE)
-	//SYSTICK MAX
-	if (SysTick_Config(val)) {
 
+	if (SysTick_Config(val)) {
 		while (1)
 			;
 	}
-#endif
 }
 
 void SystemCoreClockUpdate(uint32_t clock) {
 	uint32_t val = SysTick_LOAD_RELOAD_Msk+1;
 	SystemCoreClock = clock;
 	g_time_loop = 0;
-#if !defined(ENABLE_OS) && !defined(RTE_CMSIS_RTOS2) && !defined(RTE_RTOS_FreeRTOS_CORE)
-	//SYSTICK MAX
-	if (SysTick_Config(val)) {
 
+	if (SysTick_Config(val)) {
 		while (1)
 			;
 	}
-#endif
 }
 
-/**
- \brief  Get Current Tick.
-
- */
 void SystemGetTick(uint32_t *systick, uint32_t *loop_cnt)
 {
 	*systick = SysTick->VAL;
 	*loop_cnt = g_time_loop;
 }
 
-#if !defined(HX_TFM)
-
 void SystemInit(void) {
-
-#if defined (__VTOR_PRESENT) && (__VTOR_PRESENT == 1U)
-  SCB->VTOR = (uint32_t)(&__VECTOR_TABLE[0]);
-#endif
-
-#if (defined (__FPU_USED) && (__FPU_USED == 1U)) || \
-    (defined (__ARM_FEATURE_MVE) && (__ARM_FEATURE_MVE > 0U))
-  SCB->CPACR |= ((3U << 10U*2U) |           /* enable CP10 Full Access */
-                 (3U << 11U*2U)  );         /* enable CP11 Full Access */
-#if defined (__ARM_FEATURE_CMSE) &&  (__ARM_FEATURE_CMSE == 3U)
-  SCB_NS->CPACR |= ((3U << 10U*2U) |           /* enable CP10 Full Access */
-                 (3U << 11U*2U)  );         /* enable CP11 Full Access */
-#endif
-#endif
+	SCB->VTOR = (uint32_t)(&__VECTOR_TABLE[0]);
+	SCB->CPACR |= ((3U << 10U*2U) |           /* enable CP10 Full Access */
+		       (3U << 11U*2U));           /* enable CP11 Full Access */
+	SCB_NS->CPACR |= ((3U << 10U*2U) |        /* enable CP10 Full Access */
+		          (3U << 11U*2U));        /* enable CP11 Full Access */
 	SCB->SHCSR |= (1 << SCB_SHCSR_MEMFAULTENA_Pos);
 	SCB->SHCSR |= (1 << SCB_SHCSR_BUSFAULTENA_Pos);
 	SCB->SHCSR |= (1 << SCB_SHCSR_USGFAULTENA_Pos);
-#if defined (__ARM_FEATURE_CMSE) &&  (__ARM_FEATURE_CMSE == 3U)
-  SCB->SHCSR |= (1 << SCB_SHCSR_SECUREFAULTENA_Pos);
-#endif
+	SCB->SHCSR |= (1 << SCB_SHCSR_SECUREFAULTENA_Pos);
 
-#ifdef UNALIGNED_SUPPORT_DISABLE
-  SCB->CCR |= SCB_CCR_UNALIGN_TRP_Msk;
-#endif
-
-// Enable Loop and branch info cache
+	// Enable Loop and branch info cache
 	SCB->CCR |= SCB_CCR_LOB_Msk;
 	__ISB();
 
-
-	for(IRQn_Type idx = 0; idx <=200; idx++)
-	{
+	for(IRQn_Type idx = 0; idx <=200; idx++) {
 		NVIC_DisableIRQ(idx);
 	}
 
-#if defined (__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
-#ifndef TRUSTZONE_SEC_ONLY
-  hx_drv_scu_set_CM55M_IDAU(3, 0, 0, 1);
-#endif
-#endif
-
-  SystemCoreClockInit();
+	SystemCoreClockInit();
 }
-
-#endif //#if !defined(HX_TFM)

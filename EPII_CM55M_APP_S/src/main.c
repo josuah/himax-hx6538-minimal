6d7ec5d77c750aee50f435a25a5d63e098473270
Josuah Demangeon <me@josuah.net>
2024-12-07 09:06:07 +0000

    inline retarget

--- EPII_CM55M_APP_S/src/main.c
+++ EPII_CM55M_APP_S/src/main.c
@@ -18,28 +18,35 @@
 
 #define WE2_CHIP_VERSION_C	      0x8538000c
 
+void *_sbrk_r() { return NULL; }
+int _exit() { return 0; }
+int _read_r() { return 0; }
+int _write_r() { return 0; }
+int _close_r() { return 0; }
+int _fstat_r() { return 0; }
+int _isatty_() { return 0; }
+int _isatty() { return 0; }
+int _lseek_r() { return 0; }
+
 int main(void)
 {
 	SCU_PINMUX_CFG_T pinmux_cfg;
 	DEV_UART *console_uart = hx_drv_uart_get_dev(DW_UART_0_ID);
-	uint32_t wakeup_event;
-	uint32_t wakeup_event1;
 	uint32_t freq;
 
 	hx_drv_scu_get_all_pinmux_cfg(&pinmux_cfg);
 	pinmux_cfg.pin_pb0 = SCU_PB0_PINMUX_UART0_RX_1;
 	pinmux_cfg.pin_pb1 = SCU_PB1_PINMUX_UART0_TX_1;
 	hx_drv_scu_set_all_pinmux_cfg(&pinmux_cfg, 1);
-	hx_drv_pmu_get_ctrl(PMU_pmu_wakeup_EVT, &wakeup_event);
-	hx_drv_pmu_get_ctrl(PMU_pmu_wakeup_EVT1, &wakeup_event1);
+
 	TZ_Set_ALL_Secure();
+
 	hx_drv_scu_init();
 	hx_drv_scu_get_freq(SCU_CLK_FREQ_TYPE_HSC_CLK, &freq);
-	SystemCoreClockUpdate(freq);
-	hx_drv_uart_init(USE_DW_UART_0, HX_UART0_BASE);
-	hx_drv_uart_init(USE_DW_UART_1, HX_UART1_BASE);
-	hx_drv_uart_init(USE_DW_UART_2, HX_UART2_BASE);
 
+	SystemCoreClockUpdate(freq);
+
+	hx_drv_uart_init(USE_DW_UART_0, HX_UART0_BASE);
 	console_uart->uart_open(UART_BAUDRATE_921600);
 
 	while (1) {

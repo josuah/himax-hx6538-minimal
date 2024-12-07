156e6788521d9344fe77db34b1cf7651b91d70aa
Josuah Demangeon <me@josuah.net>
2024-12-07 08:19:35 +0000

    remove more files

--- EPII_CM55M_APP_S/src/main.c
+++ EPII_CM55M_APP_S/src/main.c
@@ -1,287 +1,48 @@
-/**************************************************************************************************
- (C) COPYRIGHT, Himax Technologies, Inc. ALL RIGHTS RESERVED
- ------------------------------------------------------------------------
- File        : main.c
- Project     : EPII
- DATE        : 2021/04/01
- AUTHOR      : 902447
- BRIFE       : main function
- HISTORY     : Initial version - 2021/04/01 created by Jacky
- **************************************************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 #include "WE2_device.h"
 #include "WE2_core.h"
-#include "board.h"
-#ifdef LIB_COMMON
 #include "xprintf.h"
+#include "hx_drv_pmu.h"
+#include "hx_drv_scu.h"
+#include "hx_drv_uart.h"
 #include "console_io.h"
-#endif
-#include "pinmux_init.h"
-#include "platform_driver_init.h"
+#include "trustzone_cfg.h"
 
-#define CIS_XSHUT_SGPIO0
-
-#ifdef HM_COMMON
-#include "hx_drv_CIS_common.h"
-#endif
-#if defined(CIS_HM0360_MONO_REVB) || defined(CIS_HM0360_MONO_OSC_REVB) \
-	|| defined(CIS_HM0360_BAYER_REVB) || defined(CIS_HM0360_BAYER_OSC_REVB) \
-	||  defined(CIS_HM0360_MONO) || defined(CIS_HM0360_BAYER)
-#include "hx_drv_hm0360.h"
-#endif
-#ifdef CIS_HM11B1
-#include "hx_drv_hm11b1.h"
-#endif
-#if defined(CIS_HM01B0_MONO) || defined(CIS_HM01B0_BAYER)
-#include "hx_drv_hm01b0.h"
-#endif
-#ifdef CIS_HM2140
-#include "hx_drv_hm2140.h"
-#endif
-#ifdef CIS_XSHUT_SGPIO0
-#define DEAULT_XHSUTDOWN_PIN    AON_GPIO2 
-#else
-#define DEAULT_XHSUTDOWN_PIN    AON_GPIO2 
-#endif
-
-
-#ifdef ALLON_SENSOR_TFLM
-#include "allon_sensor_tflm.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-
-#ifdef ALLON_SENSOR_TFLM_FATFS
-#include "allon_sensor_tflm_fatfs.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-
-#ifdef ALLON_SENSOR_TFLM_FREERTOS
-#include "allon_sensor_tflm.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-
-#ifdef HELLO_WORLD_FREERTOS_TZ_S_ONLY
-#include "hello_world_freertos_tz_s_only.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-
-#ifdef TFLM_FD_FM
-#include "tflm_fd_fm.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-
-#ifdef FATFS_TEST
-#include "fatfs_test.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	fatfs_test();
-	return 0;
-}
-#endif
-
-
-#ifdef TFLM_YOLOV8_OD
-#include "tflm_yolov8_od.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	tflm_yolov8_od_app();
-	return 0;
-}
-#endif
-
-
-#ifdef TFLM_YOLOV8_POSE
-#include "tflm_yolov8_pose.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	tflm_yolov8_pose_app();
-	return 0;
-}
-#endif
-
-
-#ifdef TFLM_2IN1_FD_FL_FR_ENROLL_YOLOV8
-#include "tflm_2in1_fd_fl_fr_enroll_yolov8.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-
-#ifdef PDM_RECORD
-#include "pdm_record.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-
-#ifdef IMU_READ_APP
-#include "imu_read_app.h"
-
-/* main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-
-#ifdef HELLO_WORLD_CMSIS_DSP
-#include "hello_world_cmsis_dsp.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-#ifdef HELLO_WORLD_CMSIS_CV
-#include "hello_world_cmsis_cv.h"
-
-/** main entry */
-int main(void)
-{
-	board_init();
-	app_main();
-	return 0;
-}
-#endif
-
-#ifdef EI_STANDALONE_INFERENCING
-#include "ei_standalone_inferencing.h"
+#define WE2_CHIP_VERSION_C	      0x8538000c
 
 int main(void)
 {
-	board_init();
-	ei_standalone_inferencing_app();
+	SCU_PINMUX_CFG_T pinmux_cfg;
+	uint32_t wakeup_event;
+	uint32_t wakeup_event1;
+	uint32_t freq;
 
-	return 0;
-}
-#endif
-
-#ifdef EI_STANDALONE_INFERENCING_CAMERA
-#include "ei_standalone_inferencing_camera.h"
-
-int main(void)
-{
-	board_init();
-	ei_standalone_inferencing_app();
-
-	return 0;
-}
-#endif
-
-#ifdef EDGE_IMPULSE_FIRMWARE
-#include "edge_impulse_firmware.h"
-
-int main(void)
-{
-	/*set pinmux init*/
-	pinmux_init();
-	/*platform driver init*/
-	platform_driver_init();
-#ifdef IP_uart
-	console_setup(USE_DW_UART_0, UART_BAUDRATE_115200);
-#endif
-#ifdef LIB_COMMON
+	hx_drv_scu_get_all_pinmux_cfg(&pinmux_cfg);
+	pinmux_cfg.pin_pb0 = SCU_PB0_PINMUX_UART0_RX_1;
+	pinmux_cfg.pin_pb1 = SCU_PB1_PINMUX_UART0_TX_1;
+	hx_drv_scu_set_all_pinmux_cfg(&pinmux_cfg, 1);
+	hx_drv_pmu_get_ctrl(PMU_pmu_wakeup_EVT, &wakeup_event);
+	hx_drv_pmu_get_ctrl(PMU_pmu_wakeup_EVT1, &wakeup_event1);
+	TZ_Set_ALL_Secure();
+	hx_drv_scu_init();
+	hx_drv_scu_get_freq(SCU_CLK_FREQ_TYPE_HSC_CLK, &freq);
+	SystemCoreClockUpdate(freq);
+	hx_drv_uart_init(USE_DW_UART_0, HX_UART0_BASE);
+	hx_drv_uart_init(USE_DW_UART_1, HX_UART1_BASE);
+	hx_drv_uart_init(USE_DW_UART_2, HX_UART2_BASE);
+	console_setup(DW_UART_0_ID, UART_BAUDRATE_921600);
 	xprintf_setup();
-#endif
-	edge_impulse_app();
 
+	while (1) {
+		xprintf("wakeup_event=0x%x,WakeupEvt1=0x%x\n", wakeup_event, wakeup_event1);
+	}
 	return 0;
 }
-#endif
-
-#ifdef KWS_PDM_RECORD
-#include "kws_pdm_record.h"
-
-/* main entry */
-int main(void)
-{
-	board_init();
-	kws_pdm_record_app();
-	return 0;
-}
-#endif
-
-
-#ifdef TFLM_PEOPLENET
-#include "tflm_peoplenet.h"
-/** main entry */
-int main(void)
-{
-	board_init();
-	tflm_peoplenet_app();
-	return 0;
-}
-#endif
-
